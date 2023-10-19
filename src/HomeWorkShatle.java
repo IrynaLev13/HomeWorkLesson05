860a106810e598e5bce61db986b3a3ea25e673ce
IrynaLev13 <ikot@ukr.net>
2023-10-19 09:30:19 +0300

    HomeWorkShatle

---
+++ src/HomeWorkShatle.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class HomeWorkShatle {
+    public static void main(String[] args) {
+        for (int a = 1; a <=100; a++){
+            if ((a%10!=4) && (a%10!=9) && (a!=4) && (a!=9) && (a/10 !=4) && (a/10 !=9)){
+                System.out.print(a + ", ");
+            }
+
+        }
+
+        }
+    }
+
