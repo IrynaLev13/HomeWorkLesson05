84ab556fcf24f378354b902cd651058ce6ee39e3
IrynaLev13 <ikot@ukr.net>
2023-10-19 11:32:14 +0300

    Add exclusion of tens

--- src/HomeWorkShatle.java
+++ src/HomeWorkShatle.java
@@ -2,8 +2,8 @@ import java.util.Random;
 
 public class HomeWorkShatle {
     public static void main(String[] args) {
-        for (int a = 1; a <=100; a++){
-            if ((a%10!=4) && (a%10!=9) && (a!=4) && (a!=9) && (a/10 !=4) && (a/10 !=9)){
+        for (int a = 1; a <=155; a++){
+            if ((a%10!=4) && (a%10!=9) && (a!=4) && (a!=9) && (a/10 !=4) && (a/10 !=9) && (a%100/10 !=4) && (a%100/10 !=9)){
                 System.out.print(a + ", ");
             }
 
